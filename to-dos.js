97406e2380b7143a32e210b2a2219ecbd867f86c
adk-saugat <saugat.adk12@gmail.com>
2024-08-28 16:44:53 -0500

    Adding and deleting task

--- to-dos.js
+++ to-dos.js
@@ -1,12 +1,15 @@
-let todos = [];
+let todos = []
 
-todos.push("Do Assignments.");
-todos.push("Clean Apartment.");
+todos.push("Do Assignments.")
+todos.push("Clean Apartment.")
+todos.push("Do Laundry.")
 
+todos.pop("Do Laundry.")
 console.log("Todos:")
 todos.forEach(function (items, index){
     console.log(`${index + 1}. ${items}`)
 })
+
 
 
 
