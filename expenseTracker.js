c1dd9e856aab0ca8a82cdac5935ff40edd0d8e12
adk-saugat <saugat.adk12@gmail.com>
2024-08-31 16:23:50 -0500

    adding Income array

--- expenseTracker.js
+++ expenseTracker.js
@@ -1,6 +1,14 @@
 const account = {
     name: 'Saugat Adhikari',
+    income: [],
     expenses : [],
+    addIncome : function (description, amount){
+        this.income.push({
+            description: description,
+            amount: amount
+        })
+    },
+
     addExpense : function (description, amount){
         this.expenses.push({
             description: description,
@@ -8,17 +16,23 @@ const account = {
         })
     },
     getAccountSummary: function (){
-        let total = 0
-        this.expenses.forEach(function (expense){
-            total += expense.amount
+        let totalIncome = 0
+        let totalExpense = 0
+
+        this.income.forEach(function (income){
+            totalIncome += income.amount
         })
-        return `${this.name} has expense of $${total}.`;
+        this.expenses.forEach(function (expense){
+            totalExpense += expense.amount
+        })
+        return `${this.name} has balance $${totalIncome - totalExpense}. $${totalIncome} in income. $${totalExpense} in expense.`;
     }
 }
 
 account.addExpense('Rent', 950)
 account.addExpense('Subway', 6)
 account.addExpense('Coffee', 2)
+account.addIncome('Job', 1000)
 console.log(account.getAccountSummary())
 
 
